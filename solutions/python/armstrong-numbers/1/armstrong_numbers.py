faed03463e2f09856e83735eefe63cee4eed7890
amine-za <aminezaghloul2003@gmail.com>
2026-03-31 22:30:19 +0100

    structure enhencements

--- solutions/python/armstrong-numbers/1/armstrong_numbers.py
+++
@@ -1,7 +0,0 @@
-def is_armstrong_number(number):
-    number_str = str(number)
-    number_len = len(number_str)
-
-    total = sum(int(char) ** number_len for char in number_str)
-
-    return number == total
--- solutions/python/leap/2/leap.py
+++
@@ -1,5 +0,0 @@
-def leap_year(year):
-    """
-        Your task is to determine whether a given year is a leap year.
-    """
-    return (year % 100 != 0 and year % 4 == 0) or year % 400 == 0
